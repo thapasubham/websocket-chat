1ecc2757e9f0f6967a504a3c252a7c4801437a1d
Subham Thapa <subhamits4.2@gmail.com>
2026-05-28 15:45:45 +0545

    k6 testing for canvas

--- ui-automation/load-testing/script.js
+++ ui-automation/load-testing/script.js
@@ -1,28 +1,61 @@
 import ws from "k6/ws";
 import { sleep } from "k6";
+import { SharedArray } from "k6/data";
 
-export const options = {
-  vus: 500,
-  duration: "10s",
+const sharedColors = new SharedArray("color palette", function () {
+  return [
+    "#000000",
+    "#ef4444",
+    "#f97316",
+    "#eab308",
+    "#22c55e",
+    "#3b82f6",
+    "#a855f7",
+    "#ec4899",
+  ];
+});
+
+const canvas_scen = {
+  executor: "ramping-vus",
+  startVUs: 0,
+  stages: [
+    { duration: "3s", target: 50 },
+    { duration: "5m", target: 499 },
+    { duration: "2s", target: 20 },
+    { duration: "2s", target: 0 },
+  ],
+  exec: "canvasTest",
 };
 
-export default function () {
-  const url = "ws://localhost:5000/socket.io/?EIO=4&transport=websocket";
+const chatTestScene = {
+  executor: "ramping-vus",
+  startVUs: 0,
+  stages: [
+    { duration: "3s", target: 50 },
+    { duration: "5s", target: 250 },
+    { duration: "2s", target: 0 },
+  ],
+  exec: "chatTest",
+};
 
+export const options = {
+  scenarios: {
+    canvas_scen,
+  },
+};
+
+export function canvasTest() {
+  sleep(Math.random() * 2);
+  const url = "ws://localhost:5000/socket.io/?EIO=4&transport=websocket";
   let roomID = null;
 
   ws.connect(url, {}, function (socket) {
     socket.on("open", () => {
-      console.log(`VU ${__VU} connected`);
-
       socket.send("40");
-
-      socket.send('42["find-room"]');
+      socket.send('42["find-draw"]');
     });
 
     socket.on("message", (raw) => {
-      console.log(`VU ${__VU} RAW:`, raw);
-
       if (raw === "2") {
         socket.send("3");
         return;
@@ -30,36 +63,88 @@ export default function () {
 
       if (raw.startsWith("42")) {
         const parsed = JSON.parse(raw.substring(2));
-
         const event = parsed[0];
         const data = parsed[1];
-
-        console.log("EVENT:", event);
 
         if (event === "room-found") {
           roomID = data.roomID;
 
-          console.log(`VU ${__VU} joined room ${roomID}`);
-
           socket.setInterval(() => {
-            socket.send(
-              `42["message",${JSON.stringify({
-                message: `hello from ${__VU}`,
-                userName: `user-${__VU}`,
-                roomID,
-              })}]`,
-            );
-          }, 1000);
-        }
+            const randomColor =
+              sharedColors[Math.floor(Math.random() * sharedColors.length)];
+            const randomSize = Math.floor(Math.random() * 40) + 1;
+            const totalPoints = Math.floor(Math.random() * 6) + 2;
+            const randomPath = [];
 
-        if (event === "message") {
-          console.log(`VU ${__VU} received:`, JSON.stringify(data));
+            for (let i = 0; i < totalPoints; i++) {
+              randomPath.push({
+                x: Math.floor(Math.random() * 800),
+                y: Math.floor(Math.random() * 600),
+              });
+            }
+
+            const drawingPayload = [
+              "objectDrawn",
+              {
+                roomID: roomID,
+                path: randomPath,
+                color: randomColor,
+                size: randomSize,
+              },
+            ];
+
+            socket.send(`42${JSON.stringify(drawingPayload)}`);
+          }, 1000);
         }
       }
     });
 
-    socket.on("close", () => {
-      console.log(`VU ${__VU} disconnected`);
+    socket.setTimeout(() => {
+      socket.close();
+    }, 200000);
+  });
+
+  sleep(1);
+}
+
+export function chatTest() {
+  sleep(Math.random() * 2);
+  const url = "ws://localhost:5000/socket.io/?EIO=4&transport=websocket";
+  let roomID = null;
+
+  ws.connect(url, {}, function (socket) {
+    socket.on("open", () => {
+      socket.send("40");
+      socket.send('42["find-room"]');
+    });
+
+    socket.on("message", (raw) => {
+      if (raw === "2") {
+        socket.send("3");
+        return;
+      }
+
+      if (raw.startsWith("42")) {
+        const parsed = JSON.parse(raw.substring(2));
+        const event = parsed[0];
+        const data = parsed[1];
+
+        if (event === "room-found") {
+          roomID = data.roomID;
+
+          socket.setInterval(() => {
+            const chatPayload = [
+              "message",
+              {
+                message: "hello",
+                userName: "tester",
+                roomID: roomID,
+              },
+            ];
+            socket.send(`42${JSON.stringify(chatPayload)}`);
+          }, 1000);
+        }
+      }
     });
 
     socket.setTimeout(() => {
